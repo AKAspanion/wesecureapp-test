7a223d35ba3bf3312e52f4a03301c001f6721dfe
Ankit Kumar <panditankit1995@gmail.com>
2020-08-04 03:08:38 +0530

    Updates

---
+++ precache-manifest.1f7d06f584004a4d8fbd760b43c4f1b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af82d6ecedd1a0bf8d9b2d6780de935",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b783c96f4590aaf813b3",
+    "url": "./static/css/main.bb105c6e.chunk.css"
+  },
+  {
+    "revision": "20066e59db1fdb1d5d90",
+    "url": "./static/js/2.cc95bc42.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.cc95bc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b783c96f4590aaf813b3",
+    "url": "./static/js/main.34f80710.chunk.js"
+  },
+  {
+    "revision": "4a420d52f031fdeece68",
+    "url": "./static/js/runtime-main.6ea3e330.js"
+  }
+]);
